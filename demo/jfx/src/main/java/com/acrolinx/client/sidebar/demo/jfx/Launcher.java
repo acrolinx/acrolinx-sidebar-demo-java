8a8c7ec8a55cb0399ddbaaf41133e839b30c0ca1
Franziska Preiß <franziska.preiss@acrolinx.com>
2020-01-20 17:12:39 +0100

    DEV-22398 Updated sidebar Demo project to show case how to include JFX
    * create separate launcher for JFX
    * added JFX Gradle Plugin
    * for platform independence load all jars necessary as compile dependencies

---
+++ demo/jfx/src/main/java/com/acrolinx/client/sidebar/demo/jfx/Launcher.java
@@ -0,0 +1,12 @@
+/* Copyright (c) 2016-present Acrolinx GmbH */
+
+package com.acrolinx.client.sidebar.demo.jfx;
+
+public class Launcher
+{
+
+    public static void main(String[] args)
+    {
+        AcrolinxDemoClientJFX.main(args);
+    }
+}
